dd27dc4dcb06b49e990334f0152990b0c5d02447
osmanbaskaya <thyme.exe@gmail.com>
2015-12-13 17:32:58 +0200

    lemmatized version

--- coarse-wsd/analyze/__init__.py
+++ coarse-wsd/analyze/__init__.py
@@ -5,9 +5,14 @@ from collections import defaultdict as dd
 import numpy as np
 from nltk.corpus.reader import BracketParseCorpusReader
 import matplotlib.pyplot as plt
+from nltk.stem import WordNetLemmatizer
 
 
-def get_stats_from_snli_dataset(files, tagset=("NN", "NNS")):
+def get_stats_from_snli_dataset(files, tagset=("NN", "NNS"), use_lemmas=False):
+
+    lemmatizer = None
+    if use_lemmas:
+        lemmatizer = WordNetLemmatizer()
 
     stats = dd(int)
     num_of_token = 0
@@ -23,6 +28,8 @@ def get_stats_from_snli_dataset(files, tagset=("NN", "NNS")):
         reader = BracketParseCorpusReader("/tmp", os.path.basename(f.name))
         for word, tag in reader.tagged_words():
             if tagset is None or tag in tagset:
+                if use_lemmas:
+                    word = lemmatizer.lemmatize(word, pos=tag.lower()[0])
                 stats[word] += 1
                 num_of_token += 1
 
@@ -44,7 +51,7 @@ def draw_coverage(stats, total):
     plt.xticks(range(100, 2010, 100))
     plt.yticks(np.linspace(0.5, 1, 11))
     plt.grid()
-    plt.savefig("../../datasets/SNLI-training-set-coverage")
+    plt.savefig("../../datasets/SNLI-training-set-coverage-lemmatized")
 
 
 def write_topn_words(stats, fn, n=2000):
@@ -58,10 +65,10 @@ def write_topn_words(stats, fn, n=2000):
 
 def draw_coverage_for_SNLI_dataset():
     files = ("../../datasets/snli_1.0/snli_1.0_train.jsonl",)
-    stats, num_of_token = get_stats_from_snli_dataset(files)
+    stats, num_of_token = get_stats_from_snli_dataset(files, use_lemmas=True)
     # LOGGER.info("# of unique word = %d \t # of different token: %d", len(stats), num_of_token)
     stats = sorted(stats.items(), key=lambda t: t[1], reverse=True)
-    write_topn_words(stats, "../../datasets/snli-top2000.txt", n=2000)
+    write_topn_words(stats, "../../datasets/snli-top2000-lemmatized.txt", n=2000)
     draw_coverage(stats, num_of_token)
 
 
