6a9ee7a90844912d3263fec67c484bc3db8ef756
osmanbaskaya <thyme.exe@gmail.com>
2015-12-19 14:49:38 +0200

    sorted by freqs done

--- coarse-wsd/utils/__init__.py
+++ coarse-wsd/utils/__init__.py
@@ -6,25 +6,27 @@ import os
 import sys
 
 
-def top_words(files, path='.', output_fn):
+def top_words(files):
     d = dd(float)
     for fn in files:
-        fn = os.path.join(path, fn)
         with open(fn) as f:
+            print >> sys.stderr, fn
             for line in f:
                 word, _, freq = line.strip().split('\t')
-                d[word] += freq
+                d[word] += float(freq)
 
-    with open(output_fn, 'w') as f:
-        for word, freq in d.iteritems():
-            f.write("%s\t%f\n".format(word, freq))
+    freq_sorted = sorted(d.iteritems(), key=lambda t: t[1], reverse=True)
+
+    for word, freq in freq_sorted:
+        print "{}\t{}".format(word, freq)
 
 def run():
     method = globals()[sys.argv[1]] 
-    args = sys.argv[2:-2]
-    path = sys.argv[-2]
-    method(args)
-
+    files = sys.argv[2:]
+    method(files)
 
 if __name__ == "__main__":
     run()
+
+
+#python __init__.py top_words ../../datasets/2013-test.filtered.gz.txt ../../datasets/2014-train-test.filtered.gz.txt ../../datasets/2015-raw-sts.filtered.gz.txt ../../datasets/snli-top2000-lemmatized.txt > freq_sorted.txt
