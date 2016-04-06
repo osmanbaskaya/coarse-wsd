a244492477afdce089559d858969f37a19f5b03f
Osman Baskaya <thyme.exe@gmail.com>
2016-04-06 23:17:11 +0300

    Lots of fixes.

--- coarse-wsd/extract/__init__.py
+++ coarse-wsd/extract/__init__.py
@@ -1,18 +1,20 @@
 import wikipedia
+import codecs
 from bs4 import BeautifulSoup
 import requests
-import sys
-from wikipedia.exceptions import PageError
-import logging
+from multiprocessing import Pool
+import os
+from wikipedia.exceptions import PageError, DisambiguationError
+from utils import configure_logger
+from collections import defaultdict as dd
 
 BASE_URL = "https://en.wikipedia.org"
 WHAT_LINKS_HERE_URL = "https://en.wikipedia.org/w/index.php?title=Special:WhatLinksHere/{}&limit={}"
 MIN_SENTENCE_SIZE = 8
 
-LOGGER = logging.getLogger(__name__)
+LOGGER = None
 
-
-def extract_instances(content, word):
+def extract_instances(content, word, pos, starting_instance_id):
     instances = []
     instances_replaced = []
     for line in content.split('\n'):
@@ -21,44 +23,59 @@ def extract_instances(content, word):
         if num_of_tokens >= MIN_SENTENCE_SIZE:
             for i in xrange(num_of_tokens):
                 if word in tokens[i].lower():
-                    instances.append(u"{} <target_word>{}</target+word> {}".format(u' '.join(tokens[:i]),
-                                                                                  tokens[i],
-                                                                                  u' '.join(tokens[i+1:])))
+                    starting_instance_id += 1
+                    instances.append(u"{} <{}.{}.{}>{}</{}.{}.{}> {}".format(u' '.join(tokens[:i]), word, pos, starting_instance_id,
+                                                                                 tokens[i], word, pos, starting_instance_id,
+                                                                                 u' '.join(tokens[i+1:])))
 
-                    instances_replaced.append(u"{} <target_word>{}</target+word> {}".format(u' '.join(tokens[:i]),
-                                                                                            word,
-                                                                                            u' '.join(tokens[i+1:])))
+                    instances_replaced.append(u"{} <{}.{}>{}</{}.{}> {}".format(u' '.join(tokens[:i]), word, pos, starting_instance_id,
+                                                                                word, word, pos, starting_instance_id,
+                                                                                u' '.join(tokens[i+1:])))
 
-    return instances, instances_replaced
+    return instances, instances_replaced, len(instances)
 
 
-def extract_from_page(page_title, word, offset, fetch_links):
-
-    print page_title
+def wiki_page_query(page_title):
     try:
+        LOGGER.debug('Retrieving {} from Wikipedia'.format(page_title))
         p = wikipedia.page(page_title)
-    except PageError as e:
-        LOGGER.info("Page '{}' not found for target word '{}'.".format(page_title, word, ))
+        return p
+    except PageError:
+        LOGGER.debug("Page '{}' not found.".format(page_title))
         # wikipedia library has a possible bug for underscored page titles.
         if '_' in page_title:
             title = page_title.replace('_', ' ')
-            LOGGER.info("Trying '{}'".format(title))
-            return extract_from_page(title, word, offset, fetch_links)
-        else:
-            return [], []
+            LOGGER.debug("Trying '{}'".format(title))
+            return wiki_page_query(title)
+    except DisambiguationError as e:
+        LOGGER.warning('DisambiguationError for {}:{}'.format(e.title, '\t'.join(e.options)))
+        return None  # This is most likely the "What links here" page and we can safely skip it.
 
-    instances, instances_replaced = extract_instances(p.content, word)
+
+def extract_from_page(page_title, word, offset, fetch_links):
+    pos = offset[-1]
+
+    p = wiki_page_query(page_title)
+    if p is None:
+        LOGGER.warning('No page found for {}'.format(page_title))
+        return [], []
+
+    instances, instances_replaced, count = extract_instances(p.content, word, pos, 0)
     if fetch_links:
         links = fetch_what_links_here(p.title)
-        for link in links[:5]:
-            link_page_title = link[6:]
-            link_page = wikipedia.page(link_page_title)
-            link_instances, link_instances_replaced = extract_instances(link_page.content, word)
-            instances.extend(link_instances), instances_replaced.extend(link_instances_replaced)
+        for link in links:
+            link_page_title = link.replace('/wiki/', '')
+            link_page = wiki_page_query(link_page_title)
+            if link_page is not None:
+                link_instances, link_instances_replaced, link_count = extract_instances(link_page.content, word, pos,
+                                                                                        len(instances))
+                instances.extend(link_instances), instances_replaced.extend(link_instances_replaced)
+
     return instances, instances_replaced
 
 
-def extract_instances_for_word(senses):
+def extract_instances_for_word(senses, wiki_dir='../datasets/wiki/'):
+    LOGGER.info("Processing word: %s" % senses[0]['word'])
     instances = []
     instances_replaced = []
     for sense_args in senses:
@@ -67,22 +84,48 @@ def extract_instances_for_word(senses):
         instances_replaced.extend(sense_instances_replaced)
 
     # TODO: create a file in ..datasets/wiki/ and write instances.
+    # original version
+    with codecs.open(os.path.join(wiki_dir, '%s.txt' % senses[0]['word']), 'w', encoding='utf8') as f:
+        f.write('\n'.join(instances))
+
+    # target word replaced version (e.g., dogs, DOG, Dog are replaced by 'dog')
+    with codecs.open(os.path.join(wiki_dir, '%s_replaced.txt' % senses[0]['word']), 'w', encoding='utf8') as f:
+        f.write('\n'.join(instances_replaced))
 
 
-def fetch_what_links_here(title, limit=500):
+def fetch_what_links_here(title, limit=100):
     url = WHAT_LINKS_HERE_URL.format(title, limit)
     links = []
-    print "Processing: ", url
+    LOGGER.debug("Processing link: %s" % url)
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         rows = soup.find(id='mw-whatlinkshere-list').find_all('li', recursive=False)
         links = [row.find('a')['href'] for row in rows]
     else:
-        print >> sys.stderr, "Error while link fetching: ", url
+        LOGGER.error("Error while link fetching: %s" % url)
 
     assert len(links) <= limit, "Fetched links are more than the limit."
     return links
+
+
+def extract_from_file(filename, num_process):
+    global LOGGER
+    from utils import LOGGER
+
+    pool = Pool(num_process)
+    jobs = dd(list)
+    for line in open(filename):
+        line = line.split()
+        target_word, page_title, offset = line[:3]
+        jobs[target_word].append(dict(word=target_word, page_title=page_title, offset=offset, fetch_links=True))
+
+    LOGGER.info("Total {} of jobs available. Consuming has been started.".format(len(jobs)))
+    pool.map(extract_instances_for_word, jobs.values())
+    # For debug:
+    # for v in jobs.values():
+    #     extract_instances_for_word(v)
+    # LOGGER.info("Done.")
 
 
 if __name__ == '__main__':
--- coarse-wsd/fetch_instances.py
+++ coarse-wsd/fetch_instances.py
@@ -1,9 +1,6 @@
-from extract import extract_instances_for_word
-import logging
 from utils import configure_logger
 import os
-from multiprocessing import Pool
-from collections import defaultdict as dd
+from extract import extract_from_file
 
 
 def run():
@@ -12,29 +9,24 @@ def run():
     parser.add_argument('--filename', required=False, default='wikipages.txt')
     parser.add_argument('--num-process', help="Number of process for parallel processing", required=False, default=1,
                         type=int)
+    parser.add_argument('--log-level', required=False, default="info")
     args = parser.parse_args()
 
-    LOGGER.info("Input file: {}".format(args.filename))
+    logger = configure_logger(args.log_level)
 
-    directory = '../datasets/wiki'
+    logger.info("Input file: {}".format(args))
+
+    directory = '../datasets/wiki/'
     try:
         os.mkdir(directory)
     except OSError:
-        LOGGER.info("{} is already exist".format(directory))
+        logger.debug("{} is already exist".format(directory))
 
-    pool = Pool(args.num_process)
-    jobs = dd(list)
-    for line in open(args.filename):
-        line = line.split()
-        target_word, page_title, offset = line[:3]
-        jobs[target_word].append(dict(word=target_word, page_title=page_title, offset=offset, fetch_links=True))
+    extract_from_file(args.filename, args.num_process)
 
-    LOGGER.info("Total {} of jobs available. Consuming has been started.".format(len(jobs)))
-    list(pool.imap_unordered(extract_instances_for_word, jobs.values()))
-    LOGGER.info("Done.")
+
+
 
 if __name__ == '__main__':
-    configure_logger()
-    LOGGER = logging.getLogger(__name__)
     run()
 
