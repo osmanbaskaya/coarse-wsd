5ea0a2b2b56884772d57a5eb647aa3ec17f20f19
Osman Baskaya <thyme.exe@gmail.com>
2016-04-20 23:45:44 +0300

    Wiki extraction pagination done.
    * Talks will be omitted in WhatLinksHere

--- coarse-wsd/extract/__init__.py
+++ coarse-wsd/extract/__init__.py
@@ -8,7 +8,8 @@ from wikipedia.exceptions import PageError, DisambiguationError
 from collections import defaultdict as dd
 
 BASE_URL = "https://en.wikipedia.org"
-WHAT_LINKS_HERE_URL = "https://en.wikipedia.org/w/index.php?title=Special:WhatLinksHere/{}&limit={}"
+# What Links Here url. Redirection pages omitted.
+WHAT_LINKS_HERE_URL = "https://en.wikipedia.org/w/index.php?title=Special:WhatLinksHere/{}&limit={}&hideredirs=1"
 MIN_SENTENCE_SIZE = 8
 
 LOGGER = None
@@ -73,9 +74,12 @@ def extract_from_page(page_title, word, offset, fetch_links):
 
     instances, instances_replaced, instances_all_replaced, count = extract_instances(p.content, word, pos, 0, p.url)
     if fetch_links:
-        links = fetch_what_links_here(p.title, limit=1)
+        links = fetch_what_links_here(p.title, limit=4000)
         for link in links:
             link_page_title = link.replace('/wiki/', '')
+            # skip talk articles.
+            if link_page_title.startswith('Talk:'):
+                continue
             link_page = wiki_page_query(link_page_title)
             if link_page is not None:
                 link_instances, link_instances_replaced, link_instances_all_replaced, link_count = extract_instances(link_page.content, word, pos,
@@ -94,7 +98,6 @@ def extract_instances_for_word(senses, wiki_dir='../datasets/wiki/'):
     instances_all_replaced = []
     sense_keys = []
     sense_key_all_replaced = []
-    retrieved_pages = []
     for sense_args in senses:
         sense_instances, sense_instances_replaced, sense_instances_all_replaced = extract_from_page(**sense_args)
         instances.extend(sense_instances)
@@ -128,20 +131,37 @@ def extract_instances_for_word(senses, wiki_dir='../datasets/wiki/'):
         f.write('\n')
 
 
-def fetch_what_links_here(title, limit=100):
-    url = WHAT_LINKS_HERE_URL.format(title, limit)
-    links = []
-    LOGGER.debug("Processing link: %s" % url)
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        rows = soup.find(id='mw-whatlinkshere-list').find_all('li', recursive=False)
-        links = [row.find('a')['href'] for row in rows]
+def get_next_page_url(soup):
+    # here we assume that next link is always in -6 index.
+    element = soup.select_one('#mw-content-text').find_all('a')[-6]
+    if element.text.startswith('next'):
+        return "{}{}".format(BASE_URL, element['href'])
     else:
-        LOGGER.error("Error while link fetching: %s" % url)
+        # No more element left.
+        return None
 
-    assert len(links) <= limit, "Fetched links are more than the limit."
-    return links
+
+def fetch_what_links_here(title, limit=1000, fetch_link_size=5000):
+    # Max fetch link size is 5000.
+
+    fetch_link_size = min(limit, fetch_link_size)
+    all_links = []
+    next_page_url = WHAT_LINKS_HERE_URL.format(title, fetch_link_size)
+    total_link_processed = 0
+    while total_link_processed < limit and next_page_url is not None:
+        LOGGER.debug("Processing link: %s" % next_page_url)
+        response = requests.get(next_page_url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            rows = soup.find(id='mw-whatlinkshere-list').find_all('li', recursive=False)
+            links = [row.find('a')['href'] for row in rows]
+            next_page_url = get_next_page_url(soup)
+            total_link_processed += len(links)
+            all_links.extend(links)
+        else:
+            LOGGER.error("Error while link fetching: %s" % next_page_url)
+
+    return all_links
 
 
 def extract_from_file(filename, num_process):
@@ -150,7 +170,6 @@ def extract_from_file(filename, num_process):
     global LOGGER
     LOGGER = utils.get_logger()
 
-    pool = Pool(num_process)
     jobs = dd(list)
     for line in open(filename):
         line = line.split()
@@ -158,10 +177,13 @@ def extract_from_file(filename, num_process):
         jobs[target_word].append(dict(word=target_word, page_title=page_title, offset=offset, fetch_links=True))
 
     LOGGER.info("Total {} of jobs available. Consuming has been started.".format(len(jobs)))
-    pool.map(extract_instances_for_word, jobs.values())
-    # For debug:
-    # for v in jobs.values():
-    #     extract_instances_for_word(v)
+    if num_process > 1:
+        pool = Pool(num_process)
+        pool.map(extract_instances_for_word, jobs.values())
+    else:
+        for v in jobs.values():
+            extract_instances_for_word(v)
+
     LOGGER.info("Done.")
 
 
