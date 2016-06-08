da108b742222fa284acca7aecf3e631c5f1fb696
Osman Baskaya <thyme.exe@gmail.com>
2016-06-08 22:44:25 +0300

    ims data creater done. test needed.

--- coarse-wsd/create_ims_dataset.py
+++ coarse-wsd/create_ims_dataset.py
@@ -1,4 +1,4 @@
-from preprocess import create_sense_dataset
+from preprocess import create_IMS_formatted_dataset
 import utils
 import os
 
@@ -7,12 +7,11 @@ def run():
     utils.configure_logger('debug')
     logger = utils.get_logger()
     input_directory = '../datasets/wiki-filtered'
-    out_directory = '../datasets/wiki-senses'
-    #files = filter(lambda f: f.endswith('.tw.txt'), os.listdir(input_directory))
+    out_directory = '../datasets/ims-10'
     files = os.listdir(input_directory)
     files = [os.path.join(input_directory, f) for f in files]
     logger.info('total number of files: %d' % len(files))
-    create_sense_dataset(files, out_directory)
+    create_IMS_formatted_dataset(files, out_directory)
     logger.info('done')
 
 
--- coarse-wsd/preprocess/__init__.py
+++ coarse-wsd/preprocess/__init__.py
@@ -1,15 +1,27 @@
 import os
-from itertools import izip, count
 import re
 import codecs
 import utils
+from sklearn.cross_validation import KFold
+import shutil
+import random
+from operator import itemgetter
 
 LOGGER = None
 
 regex = re.compile(u"<target>\w+<target>")
 
 
-def sense_mapping(files, directory_to_write):
+def create_sense_dataset(files, directory_to_write):
+    """
+    It creates a dataset by concatenating each target word with its sense. This may help to create sense aware
+    word embeddings. This method creates an input for embeddings methods (i.e. Word2Vec)
+    :param files:
+    :param directory_to_write:
+    :return:
+    """
+
+
     global LOGGER
 
     LOGGER = utils.get_logger()
@@ -42,5 +54,79 @@ def sense_mapping(files, directory_to_write):
             keyfile.write('\n')
 
 
+def transform_into_IMS_input_format(lines, out_fn, target_word):
+    """<?xml version="1.0" encoding="iso-8859-1" ?><!DOCTYPE corpus SYSTEM "lexical-sample.dtd">
+         <corpus lang='english'>
+         <lexelt item="bank.n">
+            <instance id="bank.n.bnc.00000728" docsrc="BNC">
+            <context>
+            not pay it into the <head>bank</head> until we have received the signed Deed of Covenant .
+            </context>
+            </instance>
+
+            ...
+            ...
+
+        </lexelt>
+        </corpus>
+    """
+
+    start = u"""<?xml version="1.0" encoding="iso-8859-1" ?><!DOCTYPE corpus SYSTEM "lexical-sample.dtd">
+      <corpus lang='english'>
+      <lexelt item="{}">\n""".format(target_word)
+
+    instance = u"""
+            <instance id="{}.{}" docsrc="BNC">
+            <context>
+            {}
+            </context>
+            </instance>\n"""
+
+    end = u"</lexelt>\n</corpus>\n"
+
+    with codecs.open(out_fn, 'w', encoding='utf8') as out:
+        out.write(start)
+        for i, line in enumerate(lines, 1):
+            sentence = line.split('\t')[0]
+            out.write(instance.format(target_word, i, sentence))
+        out.write(end)
 
 
+def transform_into_IMS_key_format(lines, out_fn, target_word):
+    with codecs.open(out_fn, 'w', encoding='utf8') as out:
+        for i, line in enumerate(lines, 1):
+            sense = line.split('\t')[2]
+            out.write(u"{} {}.{} {}\n".format(target_word, target_word, i, sense))
+
+
+def create_directories_for_folding(directory_to_write, k):
+    try:
+        shutil.rmtree(directory_to_write)  # remove the path and its content.
+    except OSError:
+        pass
+
+    os.mkdir(directory_to_write)
+    map(lambda fold: os.mkdir(os.path.join(directory_to_write, "fold-%d" % fold)), xrange(1, k+1))
+
+
+def create_IMS_formatted_dataset(files, directory_to_write, k=5):
+    """
+    It creates a k-fold datasets for IMS.
+    """
+    create_directories_for_folding(directory_to_write, k)
+    random.seed(42)
+    for f in files:
+        _, fn = os.path.split(f)
+        target_word = fn.split('.')[0]
+        lines = codecs.open(f, encoding='utf8').read().splitlines()
+        random.shuffle(lines)
+        kf = KFold(len(lines), k)
+        for fold, (train_idx, test_idx) in enumerate(kf, 1):
+            train = itemgetter(*train_idx)(lines)
+            test = itemgetter(*test_idx)(lines)
+            out_dir = os.path.join(directory_to_write, "fold-%d" % fold)
+            for dataset_type, dataset in (('train', train), ('test', test)):
+                out_fn_data = os.path.join(out_dir, '%s.%s.txt' % (target_word, dataset_type))
+                out_fn_key = os.path.join(out_dir, '%s.%s.key' % (target_word, dataset_type))
+                transform_into_IMS_input_format(lines, out_fn_data, target_word)
+                transform_into_IMS_key_format(lines, out_fn_key, target_word)
