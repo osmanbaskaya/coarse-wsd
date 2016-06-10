dceda781e49c71ca72f7a7247f81949bf02ca410
Osman Baskaya <thyme.exe@gmail.com>
2016-06-10 16:57:18 +0300

    Cosmetic changes.

--- coarse-wsd/ims_eval.py
+++ coarse-wsd/ims_eval.py
@@ -2,14 +2,15 @@ from ims import IMS
 import sys
 
 
-def run(tw_file, data_path):
+def run(tw_file, data_path, num_process):
+    print >> sys.stderr, tw_file, data_path, num_process
     target_words = open(tw_file).read().splitlines()
-    ims = IMS(target_words, data_path)
+    ims = IMS(target_words, data_path, num_process=int(num_process))
     print ims.evaluate(remove_intermediate=False)
 
 
 if __name__ == '__main__':
-    run(sys.argv[1], sys.argv[2])
+    run(sys.argv[1], sys.argv[2], sys.argv[3])
 
 
 
