5830f94dc1868d5b4916018a3b90a829c41c4b8e
osmanbaskaya <thyme.exe@gmail.com>
2017-02-04 15:46:03 -0500

    disambiguate part done.

--- coarse-wsd/disambiguate.py
+++ coarse-wsd/disambiguate.py
@@ -1,11 +1,29 @@
 from ims import predict
 import utils
+from multiprocessing import cpu_count
+
+
+def run():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input-dir', required=True)
+    parser.add_argument('--directory-to-write', required=True)
+    parser.add_argument('--model-dir', required=True)
+    parser.add_argument('--log-level', default='debug')
+    parser.add_argument('--num-of-process', default=cpu_count()-1, type=int)
+    args = parser.parse_args()
+
+    input_dir = args.input_dir
+    output_dir = args.directory_to_write
+    model_dir = args.model_dir
+
+    utils.configure_logger(args.log_level)
+    logger = utils.get_logger()
+    logger.debug("Args: {}".format(args))
+
+    predict(model_dir, input_dir, output_dir, num_of_process=args.num_of_process)
+    logger.info('Done')
 
 
 if __name__ == '__main__':
-    utils.configure_logger()
-    model_dir = '../datasets/models/'
-    input_dir = '/tmp/ims-mt-dataset/'
-    output_dir = '/tmp/ims-outputs/'
-    predict(model_dir, input_dir, output_dir, num_of_process=7)
-
+    run()
