f30d1f2b4f374cb096aa7654bd3cb6a495b08695
osmanbaskaya <thyme.exe@gmail.com>
2017-06-23 09:31:08 -0700

    compatability __future__ imports added

--- coarse-wsd/scripts/create_wsd_input.py
+++ coarse-wsd/scripts/create_wsd_input.py
@@ -1,11 +1,14 @@
 from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
 import re
 import sys
 import codecs
 
 input_fn = sys.argv[1]
 
-REGEX = re.compile("(\w+)\.([wb]n:\d+\w)")
+REGEX = re.compile('(\w+)\.([wb]n:\d+\w)')
 
 with codecs.open('wsd-input.tsv', 'wt', encoding='latin1') as out:
     for i, line in enumerate(codecs.open(input_fn, encoding='latin1'), 1):
